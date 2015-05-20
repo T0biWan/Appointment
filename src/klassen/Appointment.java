bc44da636e5a23aeeee5c2978d2f070a748f5efb
T0biWan <Tobi.Klatt@web.de>
2015-05-20 23:00:47 +0200

    Grundstruktur der Klasse

---
+++ src/klassen/Appointment.java
@@ -0,0 +1,33 @@
+package klassen;
+
+public class Appointment {
+	//Attribute
+	String datum;
+	String startZeit;
+	String endZeit;
+	String titel;
+	String notiz;
+	String kategorie;
+	
+	//Konstruktoren
+	public Appointment () {
+	}
+	
+	//Kopierkonstruktor
+	
+
+	
+	//Methoden
+	public String toString() {
+		return null;
+	}
+	
+	//Getter
+	
+	//Setter
+	
+	//Main
+	public static void main(String[] args) {
+		//Appointment erstellen und zeigen
+	}
+}
